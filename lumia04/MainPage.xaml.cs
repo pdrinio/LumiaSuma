39a36c06273fdb495bf021dd8d5ee9a8b6c6becb
Pedro Orgeira <poc.desarrollo@hotmail.com>
2016-04-22 20:16:34 +0200

    Primeros pasos

---
+++ lumia04/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Windows.Media.SpeechSynthesis;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace lumia04
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public SpeechSynthesizer sinte = new SpeechSynthesizer();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            reta();
+
+
+        }
+
+        private void actualizaReto(operaciones _operacion)
+        {
+            this.txbOperando1.Text = _operacion.nOperando1.ToString();
+            this.txbOperando2.Text = _operacion.nOperando2.ToString();
+            this.txbResultado.Text = "?";
+        }
+
+        private void reta()
+        {
+            operaciones _operacion = new operaciones(); //creo el objeto
+            _operacion.calculaSuma(); //que genere su juego de datos
+            actualizaReto(_operacion); //muestro en pantalla
+
+            dime("Cuál es el resultado?");
+        }
+
+        private async void dime(string message)
+        {
+            var stream = await sinte.SynthesizeTextToStreamAsync(message);
+            media.SetSource(stream, stream.ContentType);
+            media.Play();   
+
+        }
+    }
+}
---
+++ lumia04/operaciones.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lumia04
+{
+    class operaciones
+    {
+        int _nOperando1, _nOperando2, _nResultado;
+        public Random rnd = new Random();
+
+        public int nOperando1
+        {
+            get
+            {
+                return _nOperando1;
+            }
+        }
+
+        public int nOperando2
+        {
+            get
+            {
+                return _nOperando2;
+            }
+        }
+        public int nResultado
+        {
+            get
+            {
+                return _nResultado;
+            }
+        }
+
+
+        public void calculaSuma()
+        {
+          for (int i = 0; i < 1000; i++)     //vamos a randomizar mejor
+            {
+                _nOperando1 = rnd.Next(0, 1000);
+           }
+
+        _nOperando1 = rnd.Next(0, 9);
+
+            for (int i=0; i <1000; i++)     //vamos a randomizar mejor
+            {
+                rnd.Next(0,1000);
+            }
+
+            for (int i = 0; i < 1000; i++)     //vamos a randomizar mejor
+            {
+                _nOperando2 = rnd.Next(0, 1000);
+            }
+
+            _nOperando2 = rnd.Next(0, 9);
+
+            _nResultado = _nOperando1 + _nOperando2;
+        }
+
+
+        public operaciones()
+        {
+            this._nOperando1 = 0;
+            this._nOperando2 = 0;
+            this._nResultado = 0;
+        }
+
+    }
+}
